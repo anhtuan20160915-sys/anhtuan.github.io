30438fb11608196b8ade7643ecf5d9618f1fa508
anhtuan2016 <anhtuan20160915@gmail.com>
2026-09-19 07:45:57 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 mkdir -p debs
+
+if [ -f "Release" ]; then sed -i 's/\r$//' Release; fi
+
 dpkg-scanpackages -m debs /dev/null > Packages
 sed -i 's/\r$//' Packages
 bzip2 -fk Packages
+
+sed -i '/MD5Sum:/,$d' Release
+echo "MD5Sum:" >> Release
+echo " $(md5sum Packages | cut -d' ' -f1) $(stat -c%s Packages) Packages" >> Release
+echo " $(md5sum Packages.bz2 | cut -d' ' -f1) $(stat -c%s Packages.bz2) Packages.bz2" >> Release
+sed -i 's/\r$//' Release
